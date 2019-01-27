7164ff0afa5c86aa5e0cdfe15591560fb007d36b
Felipe Montoya <fmbosox@gmail.com>
2019-01-26 19:19:55 -0800

    UI version 1.0

---
+++ Assets/Scripts/LoadSceneOnClicked.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadSceneOnClicked : MonoBehaviour
+{
+   
+    public void loadScene(int scene = 1)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+
+
+
+}
